4bbb18f0df6b13cde96e4218ca636869b9c123f6
Abduvohid <vohid4970@gmail.com>
2021-12-18 15:47:09 +0500

    second next create password and vue data value

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,8 +1,13 @@
 <template>
   <form>
     <label>Email:</label>
-    <input type="email" required />
+    <input type="email" required v-model="email" />
+
+    <label>Password:</label>
+    <input type="password" required v-model="password" />
   </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
 </template>
 
 <script>
@@ -10,6 +15,7 @@ export default {
   data() {
     return {
       email: "",
+      password: "",
     };
   },
 };
