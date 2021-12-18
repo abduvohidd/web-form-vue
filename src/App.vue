5e56143395916582c62521e4aafa02da42601d13
Abduvohid <vohid4970@gmail.com>
2021-12-18 15:33:56 +0500

    frit work create form and inpit html - css

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <SingupForm />
+</template>
+
+<script>
+import SingupForm from "./components/SignupForm.vue";
+export default {
+  name: "App",
+  components: {
+    SingupForm,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
